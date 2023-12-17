e62b26cb8114c1c08ad1cb7a57672ec74e40af82
Liza-Nikitina <mikhnoliza@yandex.ru>
2023-12-17 14:51:13 +0300

    version 2

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-BASIC_URL = "https://66f59063-c668-4d31-9f54-8de14fcead6b.serverhub.praktikum-services.ru"
+BASIC_URL = "https://ec3e45aa-de30-4b37-b180-61ecf2cdb48a.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders"
 GET_ORDER_PATH = "/v1/orders/track?t="
 
